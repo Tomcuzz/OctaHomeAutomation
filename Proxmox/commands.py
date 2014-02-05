ce449ff1dde7cdb982a5a727ead3195c74692ab4
Thomas Cousin <thomas.cousin@me.com>
2014-02-05 21:16:40 +0000

    Proxmox Page Running Improvements

--- Proxmox/commands.py
+++ Proxmox/commands.py
@@ -2,7 +2,37 @@ from django.http import HttpResponse
 
 def runcommand(request, proxmox):
 	command = request.GET.get('command', 'none')
-	if command == 'addcomplete':
+	if command == 'startCT':
+		node = str(equest.POST.get('node', ''))
+		ct = str(request.POST.get('ct', ''))
+		proxmox.nodes(node).openvz(ct).status.start.post()
+		return HttpResponse("Ok")
+	elif command == 'stopCT':
+		node = str(equest.POST.get('node', ''))
+		ct = str(request.POST.get('ct', ''))
+		proxmox.nodes(node).openvz(ct).status.stop.post()
+		return HttpResponse("Ok")
+	elif command == 'shutdownCT':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'migrateCT':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'consoleCT':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'removeCT':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'startVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'stopVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'shutdownVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'migrateVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'consoleVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'removeVM':
+		raise NameError('Yet To Be Implemented')
+	elif command == 'addcomplete':
 		nodeLocation = request.POST.get('node', '')
 		vmid = request.POST.get('vmid', '')
 		template = request.POST.get('template', '')
