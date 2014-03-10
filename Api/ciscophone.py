e17b06766264258c28b976e286fc5227d012c293
Thomas Cousin <thomas.cousin@me.com>
2014-03-10 03:48:04 +0000

    More Phone Interface Updates

--- Api/ciscophone.py
+++ Api/ciscophone.py
@@ -3,15 +3,20 @@ from SharedFunctions.models import *
 from Lights.models import *
 
 def HandlePhoneRequest(request):
-	print request
 	area = request.GET.get('area', 'None')
 	if area == 'None':
 		return PhoneHomePage(request)
 	elif area == 'lights':
-		if request.GET.get('room', 'None') == 'None':
-			return PhoneLightsRoomPage(request)
+		if request.GET.get('room', 'None') != 'None':
+			if request.GET.get('light', 'None') != 'None':
+				if request.GET.get('command', 'None') != 'None':
+					return PhoneLightSetRGBPage(request)
+				else:
+					return PhoneLightPage(request)
+			else:
+				return PhoneLightsPage(request)
 		else:
-			return PhoneLightsPage(request)
+			return PhoneLightsRoomPage(request)
 	else:
 		return PhoneHomePage(request)
 
@@ -20,16 +25,12 @@ def PhoneHomePage(request):
 		{'title':'Alarm', 'address':'?page=ciscophone&area=alarm'}, 
 		{'title':'Temperature', 'address':'?page=ciscophone&area=temp'}]
 	return render(request, 'pages/Api/PhoneMenu.html', {'Items':items, 'Prompt':'Please Select A Service'}, content_type="text/xml")
-	response = HttpResponse(toReturn, content_type="text/xml")
-	return response
 
 def PhoneLightsRoomPage(request):
 	items = [{'title':'All Rooms', 'address':'?page=ciscophone&area=lights&room=allrooms'}]
 	for room in Rooms.objects.all():
 		items.append({'title':room.Name.replace("_", " "), 'address':'?page=ciscophone&area=lights&room=' + str(room.id)})
 	return render(request, 'pages/Api/PhoneMenu.html', {'Items':items, 'Prompt':'Please Select A Service'}, content_type="text/xml")
-	response = HttpResponse(toReturn, content_type="text/xml")
-	return response
 
 def PhoneLightsPage(request):
 	items = []
@@ -40,7 +41,18 @@ def PhoneLightsPage(request):
 		theRoom = Rooms.objects.get(id=int(room))
 		lights = Lights.objects.filter(Room=theRoom)
 	for light in lights:
-		items.append({'title':light.LightName.replace("_", " "), 'address':'?page=ciscophone&area=lights&room=' + str(room) + '&light=' + str(light.LightName)})
+		items.append({'title':light.LightName.replace("_", " "), 'address':'?page=ciscophone&area=lights&room=' + str(room) + '&light=' + str(light.id)})
 	return render(request, 'pages/Api/PhoneMenu.html', {'Items':items, 'Prompt':'Please Select A Light', 'softkey1':'test'}, content_type="text/xml")
-	response = HttpResponse(toReturn, content_type="text/xml")
-	return response
+
+def PhoneLightPage(request):
+	light = request.GET.get('light', 'None')
+	items = [{'title':'Toggle Light', 'address':'?page=ciscophone&area=lights&room=allrooms&light=' + light + '&command=toggle'},
+		{'title':'Set RGB Values', 'address':'?page=ciscophone&area=lights&room=allrooms&light=' + light + '&command=setrgb'},
+		{'title':'Select Scene', 'address':'?page=ciscophone&area=lights&room=allrooms&light=' + light + '&command=selectscene'}]
+	return render(request, 'pages/Api/PhoneMenu.html', {'Items':items, 'Prompt':'Please Select A Service'}, content_type="text/xml")
+
+def PhoneLightSetRGBPage(request):
+	items = [{'DisplayName':'Set Red Value', 'QueryStringParam':'r', 'DefaultValue':'255', 'InputFlag':'N'},
+		{'DisplayName':'Set Green Value', 'QueryStringParam':'g', 'DefaultValue':'255', 'InputFlag':'N'},
+		{'DisplayName':'Set Blue Value', 'QueryStringParam':'b', 'DefaultValue':'255', 'InputFlag':'N'}]
+	return render(request, 'pages/Api/PhoneValueSet.html', {'Items':items, 'Prompt':'Please Select A Service', 'Url':'setrgb.xml'}, content_type="text/xml")
