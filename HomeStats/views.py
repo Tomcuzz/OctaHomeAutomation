be471761550e75582d4a1b930ffc1e2d31324fde
Thomas Cousin <thomas.cousin@me.com>
2014-03-20 01:28:53 +0000

    Stats Update

--- HomeStats/views.py
+++ HomeStats/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.shortcuts import redirect
 
+from Lights.models import *
+from DeviceInput.models import *
+
 def HomeStatsMain(request):
 	if not request.user.is_authenticated():
-                return redirect('/Login?next=%s' % request.path)
-        else:
-		title = "Under Construction"
-		stuff = "This Page Is Currently Under Construction"
-		return render(request, 'pages/HomeStats.html', {'PageAreaTitle':title, 'PageAreaContent': stuff})
+		return redirect('/Login?next=%s' % request.path)
+	
+	stats = {'lightsOn':str(Lights.objects.filter(LightState='On').count()),
+		'motionDevicesArmed':str(MotionInputDevice.objects.filter(Armed=True).count()),
+		'motionDevicesActivated':str(MotionInputDevice.objects.filter(Activated=True).count())
+		}
+	
+	lights = Lights.objects.all()
+	motionDevices = MotionInputDevice.objects.all()
+	
+	return render(request, 'pages/HomeStats.html', {'Lights':lights, 'Stats':stats})
