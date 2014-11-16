2da2cb4d1f7ea5e85f57cfd40cc148abf817e2a2
Thomas Cousin <thomas.cousin@me.com>
2014-11-16 02:46:17 +0000

    Remove south control scripts

--- SupportingFiles/SouthResetFiles.sh
+++
@@ -1,5 +0,0 @@
-array=(Home HomeStats Security Lights Alarm Curtains TempControl AudioVisual Food DeviceInput Proxmox DnsAdmin Account Weather ErrorPages SharedFunctions Api)
-for i in "${array[@]}"
-do
-	rm -rf ../$i/migrations
-done
--- SupportingFiles/SouthUpdate.sh
+++
@@ -1,7 +0,0 @@
-array=(Home HomeStats Security Lights Alarm Curtains TempControl AudioVisual Food DeviceInput Proxmox DnsAdmin Account Weather ErrorPages SharedFunctions Api)
-for i in "${array[@]}"
-do
-	echo $i
-	python ../manage.py schemamigration $i --auto
-	python ../manage.py migrate $i
-done
