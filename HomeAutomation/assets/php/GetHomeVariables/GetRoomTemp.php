d8992f26273598b0fb46d491a8998ee154017549
Thomas Cousin <thomas.cousin@me.com>
2014-11-17 00:10:45 +0000

    Remove old php files from old php version

--- HomeAutomation/assets/php/GetHomeVariables/GetRoomTemp.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once('/HomeControlPhpScripts/SQLFunctions.php');
-	
-	$roomName = $_GET["room"];
-	$rooms = getArrayForQuery("SELECT * FROM `Rooms` WHERE `Name`='$roomName'");
-    $room = $rooms[0];
-    echo $room["Temperature"];
-?>
