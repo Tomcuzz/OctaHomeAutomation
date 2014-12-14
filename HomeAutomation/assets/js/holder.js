8db2984ff48ae4732b0c016a3c339f8c1e024ef0
Thomas Cousin <tc@studio-40.com>
2014-12-14 20:10:55 +0000

    Removal of depricated Bootstrap 2.3.1 files

--- HomeAutomation/assets/js/holder.js
+++ HomeAutomation/assets/js/holder.js
@@ -1,6 +1,6 @@
+/*
 
-
-/*Holder - 1.9 - client side image placeholders
+Holder - 1.9 - client side image placeholders
 (c) 2012-2013 Ivan Malopinsky / http://imsky.co
 
 Provided under the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
