bf63befc9a6c5776ecb4b7543a60cf47ab4ec72c
Thomas Cousin <tc@studio-40.com>
2014-12-21 20:28:42 +0000

    Removal of unneded light actions

--- Lights/views.py
+++ Lights/views.py
@@ -51,31 +51,7 @@ class handleLightCommand(commandRequestHandler):
 	def runCommand(self):
 		light = self.getLight()
 		if light:
-			if self.Command == 'toggleLightState':
-				if light.IsOn:
-					light.setOnOff(False)
-				else:
-					light.setOnOff(True)
-				return self.returnOk()
-				
-			elif self.Command == 'turnOn':
-				light.setOnOff(True)
-				return self.returnOk()
-			elif self.Command == 'turnOff':
-				light.setOnOff(False)
-				return self.returnOk()
-			
-			elif self.Command.startswith('set'):
-				return self.setColour(light)
-			
-			elif self.Command == 'getR':
-				return HttpResponse(light.R)
-			elif self.Command == 'getG':
-				return HttpResponse(light.G)
-			elif self.Command == 'getB':
-				return HttpResponse(light.B)
-				
-			elif self.Command == 'getState':
+			if self.Command == 'getState':
 				return HttpResponse(json.dumps({"R":light.R, "G":light.G, "B":light.B, "Scroll":light.getScroll()}))
 			else:
 				return self.handleUserError('Command Not Recognised')
@@ -93,30 +69,6 @@ class handleLightCommand(commandRequestHandler):
 		else:
 			return self.handleUserError('Light Not Found')
 	
-	def setColour(self, light):
-		if self.Command.endswith('RGB'):
-			if self.Post.has_key('R') and self.Post.has_key('G') and self.Post.has_key('B'):
-				result = light.setRGB(self.Post['R'], self.Post['G'], self.Post['B'])
-			else:
-				return self.handleUserError('No Value Given')
-		else:
-			if self.Post.has_key('value'):
-				if self.Command.endswith('R'):
-					result = light.setR(int(self.Post['value']))
-				elif self.Command.endswith('G'):
-					result = light.setG(int(self.Post['value']))
-				elif self.Command.endswith('B'):
-					result = light.setB(int(self.Post['value']))
-				else:
-					return self.handleUserError('Command Not Recognised')
-			else:
-				return self.handleUserError('No Value Given')
-		
-		if result:
-			return self.returnOk()
-		else:
-			self.handleUserError('Please Enter A Value Between 0 and 255')
-	
 	def getLight(self):
 		light = None
 		if self.Kwarguments.has_key('lightType') and self.Kwarguments.has_key('lightId'):
@@ -124,12 +76,3 @@ class handleLightCommand(commandRequestHandler):
 				if str(aLight.id) == str(self.Kwarguments['lightId']) and (aLight.__class__.__name__ == self.Kwarguments['lightType']):
 					light = aLight
 		return light
-
-
-#if deviceType == "Arduino":
-#			message = "r=" + str(r) + ",g=" + str(g) + ",b=" + str(b) + ",t=" + str(scrollTime) + ","
-#			CommunicationControl().sendUDPMessage(ipAddress, 100, message)
-#		elif deviceType == "MaxPi":
-#			urlLocation = "/colour?r=" + str(r) + "&g=" + str(g) + "&b=" + str(b) + "&delay=" + str(int(float(scrollTime) * 100))
-#			port = 8080
-#			CommunicationControl().sendHTTPGetRequest(ipAddress, port, urlLocation)
