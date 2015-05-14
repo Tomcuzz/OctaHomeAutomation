a88643d276c39d4c370642fdb14caa7d38938127
Thomas Cousin <tc@studio-40.com>
2015-05-14 23:23:12 +0100

    Updates to light model

--- Lights/models.py
+++ Lights/models.py
@@ -107,8 +107,11 @@ class RGBLight(LightDevice):
 	def setIsOn(self, setOn):
 		if setOn == 'True':
 			if self.Colour != "#000000" and self.Colour != "#000" and self.Colour != "":
+				
+				print self.Colour
 				self.setColour(self.Colour)
 			else:
+				print "fff"
 				self.setColour("#FFFFFF")
 			self.IsOn = True
 			self.save()
@@ -138,13 +141,38 @@ class RGBLight(LightDevice):
 		if not re.match('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$', colour):
 			return False
 		
-		self.Colour = colour
+		if colour != "#000000" and colour != "#000":
+			self.Colour = colour
+		
 		self.save()
 		return True
 	
 	def setScroll(self, scrollModeName):
 		self.Scroll = LightScrollMode.objects.get(Name=scrollModeName)
 		self.save()
+	
+	def rgbColour(self):
+		value = self.Colour.lstrip('#')
+		lv = len(value)
+		return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+	
+	def getDisplayR(self):
+		if self.IsOn:
+			return self.rgbColour()[0]
+		else:
+			return 0
+	
+	def getDisplayG(self):
+		if self.IsOn:
+			return self.rgbColour()[1]
+		else:
+			return 0
+	
+	def getDisplayB(self):
+		if self.IsOn:
+			return self.rgbColour()[2]
+		else:
+			return 0
 	
 	########
 	# Meta #
