1aca0af9e23725daafb7a72d5ec1fa542eeed7e4
Thomas Cousin <tc@studio-40.com>
2015-05-17 21:51:20 +0100

    Updates to make device side bar

--- OctaHomeCore/deviceviews.py
+++ OctaHomeCore/deviceviews.py
@@ -22,6 +22,32 @@ class DeviceTopNavBarItem(TopNavBarItem):
 
 #Side Bar Menu
 class DeviceSideBarMenuObjectProvider(RoomClassMenuObjectProvider):
+	def getPreItems(self):
+		allObject = DynamicMenuItem()
+		allObject.DisplayName = "All"
+		
+		if self.SectionName != "":
+			allObject.LinkValue = reverse('GenericDeviceSection', kwargs={'deviceType':self.SectionName})
+		else:
+			allObject.LinkValue = reverse('GenericDeviceSection', kwargs={'deviceType':'All'})
+		
+		allObject.ViewPartial = self.ItemPartial
+		allObject.Request = self.Request
+		return [allObject]
+	
+	def getPostItems(self):
+		allObject = DynamicMenuItem()
+		allObject.DisplayName = "Add New Device"
+		
+		if self.SectionName != "":
+			allObject.LinkValue = reverse('AddDeviceView', kwargs={'slug':self.SectionName})
+		else:
+			allObject.LinkValue = reverse('AddDeviceView', kwargs={'slug':'All'})
+		
+		allObject.ViewPartial = self.ItemPartial
+		allObject.Request = self.Request
+		return [allObject]
+	
 	def getUrlForHouse(self, house):
 		if self.SectionName != "":
 			return reverse('GenericDeviceSection', kwargs={'deviceType':self.SectionName, 'house':house.pk})
