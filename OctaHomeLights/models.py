99729d6d1b3c19723db66c936132b599ae612037
Thomas Cousin <thomas.cousin@me.com>
2016-04-29 05:38:44 +0100

    Update to the dimable light class

--- OctaHomeLights/models.py
+++ OctaHomeLights/models.py
@@ -1,5 +1,6 @@
 import re
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from OctaHomeCore.models.device import *
 from OctaHomeCore.communication_controller import *
 
@@ -59,7 +60,7 @@ class DimmableLight(LightDevice):
 	##############
 	# Parameters #
 	##############
-	Level = models.PositiveSmallIntegerField(default=0, max_value=255)
+	Level = models.PositiveSmallIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(255)])
 	
 	#################
 	# Class Methods #
